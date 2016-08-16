3a94eb455483d81bb2ff21d28e814fafa2c5e8c3
panqiong <qiong.pan@lifesense.com>
2016-08-16 18:10:09 +0800

    xxxx

--- src/main/java/com/dubbo/consumer/DemoConsumer.java
+++ src/main/java/com/dubbo/consumer/DemoConsumer.java
@@ -4,24 +4,24 @@ import java.util.List;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.provider.user.UserService;
+//import com.provider.user.UserService;
 
 public class DemoConsumer {
 	public static void main(String[] args) throws Exception {  
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "spring/spring.xml" });
-        context.start();
-  
-        UserService userService = (UserService) context.getBean("userService"); 
-        String hello = userService.sayHello("tom");
-        System.out.println(hello);  
-        
-        List list = userService.getUsers();
-        if (list != null && list.size() > 0) {
-            for (int i = 0; i < list.size(); i++) {
-                System.out.println(list.get(i));
-            }
-        }
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+//                new String[] { "spring/spring.xml" });
+//        context.start();
+//
+//        UserService userService = (UserService) context.getBean("userService");
+//        String hello = userService.sayHello("tom");
+//        System.out.println(hello);
+//
+//        List list = userService.getUsers();
+//        if (list != null && list.size() > 0) {
+//            for (int i = 0; i < list.size(); i++) {
+//                System.out.println(list.get(i));
+//            }
+//        }
        
         System.in.read();  
     }
