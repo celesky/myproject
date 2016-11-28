9e6969e31092ac9adf808b4f1c1ae8f7a4909d52
panqiong <qiong.pan@lifesense.com>
2016-11-28 14:36:50 +0800

    aaa

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -1,5 +1,6 @@
 package com;
 
+import javax.lang.model.SourceVersion;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +8,7 @@ public class test {
 	private static final int _1MB = 1024 * 1024;
 
 	/**
-	 * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+	 * VM??????-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
 	  */
 	public static void testAllocation() throws Exception{
 	 	byte[] allocation1, allocation2, allocation3, allocation4,allocation5,allocation6;
@@ -16,10 +17,11 @@ public class test {
 		 	allocation2 = new byte[2 * _1MB];
 		 	allocation3 = new byte[6 * _1MB];
 		 	//allocation4 = new byte[2 * _1MB];
-		 	//allocation4 = new byte[4 * _1MB];  // 出现一次Minor GC
-		 	//allocation5 = new byte[2 * _1MB];  // 出现一次Minor GC
-		 	//allocation5 = new byte[4 * _1MB];  // 出现一次Minor GC
+			//allocation4 = new byte[4 * _1MB];  // ???????Minor GC
+			//allocation5 = new byte[2 * _1MB];  // ???????Minor GC
+			//allocation5 = new byte[4 * _1MB];  // ???????Minor GC
 		 	Thread.sleep(10000);
+			System.out.println("111 = " + 111);
 	 	}
 	 }
 
