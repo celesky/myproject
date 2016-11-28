3323e1961b977a4188acf986d2c37ad6e0507c2c
panqiong <qiong.pan@lifesense.com>
2016-11-28 14:31:38 +0800

    no message

---
+++ src/main/java/com/classloader/TestGit.java
@@ -0,0 +1,10 @@
+package com.classloader;
+
+/**
+ * Created by pan on 16/11/28.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("1 = " + 1);
+    }
+}
